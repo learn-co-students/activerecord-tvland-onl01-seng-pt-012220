05ba1402ebdd1cdb07642411f406fe172a037dfa
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-04 14:07:04 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,21 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor 
+  belongs_to :show
   
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+  
+  # def build_show(hash={})
+  #   show = Show.new
+  #   show.name = hash.fetch(:name)
+  #   show.characters << self
+  # end
+  
+  def build_network(hash={})
+    net = Network.new
+    net.call_letters = hash.fetch(:call_letters)
+    self.show.network = net
+  end
+
 end
