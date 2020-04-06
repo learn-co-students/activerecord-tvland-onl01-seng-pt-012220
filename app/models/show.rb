7efd6543583837f85fb09c6b5757cca02a78e527
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-06 00:45:44 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,9 +4,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-      self.actors.map do |c|
-       "#{c.first_name} #{c.last_name}"
-     end.join
-    end
+     self.actors.map do |a|
+       a.full_name
+     end
+   end
 
   end
--- spec/show_spec.rb
+++ spec/show_spec.rb
@@ -30,7 +30,7 @@ describe Show do
     show.characters << character
     show.save
 
-
+    expect(show.actors_list.length).to eq(1)
     expect(show.actors_list).to include('Andrew Lincoln')
 
   end
